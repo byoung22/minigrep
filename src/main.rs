5a44f9796a80e60488b5e8f2760bbc64e534d395
baileyyo <111161049+byoung22@users.noreply.github.com>
2025-01-06 18:13:22 -0800

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        return Ok(Config { query, file_path });
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("with text:\n{contents}");
 }
